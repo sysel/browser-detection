bd1db38e4ce6f7db0c95e1d7e03e55bcd022c787
Vojtech Sysel <vojtech.sysel@email.cz>
2017-11-22 16:05:52 +0100

    Skeleton

--- src/BrowserDetector.php
+++ src/BrowserDetector.php
@@ -6,11 +6,36 @@ use BrowserDetector\Browser;
 
 class BrowserDetector
 {
+    const OS_UNKNOWN = 'unknown',
+          OS_WINDOWS = 'Windows',
+          OS_LINUX = 'Linux';
+    const BROWSER_UNKNOWN = 'unknown';
+
     public function __construct() {
     }
 
     public function detect($userAgent) {
         $browser = new Browser;
+        $this->detectOS($userAgent, $browser);
+        $this->detectBrowser($userAgent, $browser);
+        $this->detectPlatform($userAgent, $browser);
         return $browser;
+    }
+
+    private function detectOS($userAgent, $browser) {
+        if (stristr($userAgent, 'windows')) {
+            $browser->os = self::OS_WINDOWS;
+        } elseif (stristr($userAgent, 'linux')) {
+            $browser->os = self::OS_LINUX;
+        } else {
+            $browser->os = self::OS_UNKNOWN;
+        }
+    }
+
+    private function detectBrowser($userAgent, $browser) {
+        $browser->browser = self::BROWSER_UNKNOWN;
+    }
+
+    private function detectPlatform($userAgent, $browser) {
     }
 }
